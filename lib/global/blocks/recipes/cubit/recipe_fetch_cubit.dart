0d417477a85daa417ffeda332522584427078491
kkoskenvirta <kasper.koskenvirta@qalmari.fi>
2022-07-15 08:21:39 +0300

    initial commit

---
+++ lib/global/blocks/recipes/cubit/recipe_fetch_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_e_commerce/repositorys/recipes_repository.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:flutter_e_commerce/models/recipe/failure_model.dart';
+import 'package:flutter_e_commerce/models/recipe/recipe_model.dart';
+
+part 'recipe_fetch_state.dart';
+
+class RecipeFetchCubit extends Cubit<RecipeFetchState> {
+  RecipeFetchCubit({required this.recipesRepository}) : super(RecipeFetchInitial());
+
+  final RecipesRepository recipesRepository;
+
+  Future<void> fetchRecipeApi() async {
+    emit(RecipeFetchLoading());
+    try {
+      final List<RecipeModel>? recipeList = await recipesRepository.getRecipesList();
+
+      emit(RecipeFetchLoaded(recipeList: recipeList ?? []));
+    } on Failure catch (err) {
+      emit(RecipeFetchError());
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+}
